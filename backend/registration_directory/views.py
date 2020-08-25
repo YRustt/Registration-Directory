50ffedbab2a0d643693dd081ae4e06a54bd4ccba
Rustam Yunusov <yrustt@yandex.ru>
2020-08-25 15:05:32 +0300

    Added list and create views for department.

--- backend/registration_directory/views.py
+++ backend/registration_directory/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Department, Person
+from .serializers import DepartmentListSerializer, DepartmentDetailSerializer
+
+
+class DepartmentListView(generics.ListAPIView):
+    serializer_class = DepartmentListSerializer
+
+    def get_queryset(self):
+        main_department = self.request.GET.get("main_department", None)
+        if main_department is None:
+            queryset = Department.objects.filter(main_departments__isnull=True)
+        else:
+            queryset = Department.objects.filter(main_departments__id=main_department)
+
+        return queryset
+
+
+class DepartmentCreateView(generics.CreateAPIView):
+    serializer_class = DepartmentDetailSerializer
